579b702f952a3ce55013ee6d87ab6d2fc2b196c1
100528339BC <100528339@schools.edin.org>
2021-01-20 14:37:15 +0000

    finished program

--- Main.java
+++ Main.java
@@ -1,21 +1,19 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    
-    //Step 1: initialise names array
-  
+  Scanner scan = new Scanner(System.in);
+
     String [] names = {"Marisol","Gaynelle","Josue","Cletus","Dong","Lucille","Margot","Mai","Vaughn","Sebastian","Petrina","Maxima","Fletcher","Kenda","Doug","Dot","Jeromy","Jerald","Alla","Georgene"};
    
-    //Step 2: ask user for the number to seperate students by
-  
-    //Step 3: Start fixed loop length of names array times
-  
-    //Step 4: if loop counter modulus the number entered is 0 then
-  
-    //Step 5: display the name from the array
-  
-    //Step 6: end if
-  
-    //Step 7: end loop
+    System.out.println("enter number");
+    int number = scan.nextInt();
+
+    for(int i = 0; i < 20; i ++){
+      if (i%number == 0){
+        System.out.println(""+names[i]);
+      }
+    }
+    
 
   }
 }
